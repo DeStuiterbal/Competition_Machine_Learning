b61747a7de3cd2b1508e92658f9ca2b2b87108e7
svermeulen <vermeulen.stijn06@gmail.com>
2026-04-07 09:39:01 +0200

    added some default code, added notebook for testing

--- __init__.py
+++ __init__.py
@@ -1,8 +1,15 @@
 import sklearn
+import pandas as pd
 
+
+def read_data():
+    data = "./data-studenten.csv"
+    student_data = pd.read_csv(data)
+    return student_data
 
 def model_factory():
     learner = sklearn.linear_model.Ridge()
+    learner.fit()
     return learner
 
 
